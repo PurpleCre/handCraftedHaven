52c105bf863ee278038efd7bc56c6cbb1d555029
John Mark Manuel <manueljm016@gmail.com>
2025-02-08 11:47:49 +0800

    SEO title for create profile updated

--- hand-crafted-haven/app/user/create/page.tsx
+++ hand-crafted-haven/app/user/create/page.tsx
@@ -3,7 +3,7 @@ import Form from '@/app/ui/profiles/create-form';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Create Invoice',
+    title: 'Create Profile',
 };
  
 export default async function Page() {
