dc9de78de54faec31c5ea02a615b5d4897669c6a
Cipher715 <atsushi.karino@gmail.com>
2025-02-21 03:36:41 +0900

    small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
     template: '%s | Hand Crafted Haven Dashboard',
     default: 'Hand Crafted Haven Dashboard',
   },
+  icons: {
+    icon: "/favicon.ico", 
+  },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
