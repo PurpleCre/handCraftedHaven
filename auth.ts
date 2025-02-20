dd4ed972b0e12894d92af369261609e761c85277
Cipher715 <atsushi.karino@gmail.com>
2025-02-21 03:05:08 +0900

    Revert "Added Seller Profile page"

--- auth.ts
+++ auth.ts
@@ -1,19 +1,10 @@
 import NextAuth from 'next-auth';
-// import { authConfig } from './auth.config';
+import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from './app/lib/definitions';
 import bcrypt from 'bcrypt';
-import type { NextAuthConfig } from 'next-auth';
-// import useLS from './useLS';
-
-// function SetLocalStorage(param: string){
-//   const username = param
-//   localStorage.setItem('myCount', username);
-// }
-
-
 
 async function getUser(username: string): Promise<User | undefined> {
     try {
@@ -26,7 +17,7 @@ async function getUser(username: string): Promise<User | undefined> {
 }
  
 export const { auth, signIn, signOut } = NextAuth({
-  // ...authConfig,
+  ...authConfig,
   providers: [Credentials({
     async authorize(credentials) {
         const parsedCredentials = z
@@ -36,65 +27,14 @@ export const { auth, signIn, signOut } = NextAuth({
         if (parsedCredentials.success) {
             const { username, password } = parsedCredentials.data;
             const user = await getUser(username);
-            // SetLocalStorage(username);
-            // useLS("username", username)
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password_hash);
  
-            if (passwordsMatch) return {
-              id: user.id,
-              username: user.username,
-              email: user.email,
-              full_name: user.full_name,
-              role: user.role
-          };
+            if (passwordsMatch) return user;
         }
 
         console.log('Invalid credentials');
         return null;
     }
   })],
-  callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/market');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/market', nextUrl));
-      }
-      return true;
-    },
-    
-    async jwt({ token, user }) {
-      if (user) {
-      token.id = user.id as string;
-      token.username = user.username as string;
-      token.email = user.email as string;
-      token.full_name = user.full_name as string;
-      token.role = user.role as string;
-
-      }
-      return token;
-    },
-
-    async session({ session, token }) {
-      if(session){
-      session.user.id = token.id as string;
-      session.user.username = token.username as string;
-      session.user.email = token.email as string;
-      session.user.full_name = token.full_name as string;
-      session.user.role = token.role as string;
-      }
-      return session;
-    }
-  },
-  session: {
-    strategy: 'jwt', // Use JWT for session management
-  },
-
-  pages: {
-    signIn: '/login',  // Custom login page
-  }
-}satisfies NextAuthConfig) 
+});
