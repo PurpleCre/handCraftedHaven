27179ccb8cfbfc7060ecbbd38124b3f534fdc8b0
supercoolfire <bahuiput+github@gmail.com>
2025-02-20 10:13:19 +0800

    Replaced logo with better image solving lint error

--- app/ui/hch-logo.tsx
+++ app/ui/hch-logo.tsx
@@ -1,11 +1,10 @@
-import { lusitana } from '@/app/ui/fonts';
+import Image from "next/image";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function HandLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <img src="./favicon.ico" alt="" className="h-12 w-12 rotate-[15deg]" />
+    <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`} >
+      <Image src="/logo.png" alt="Logo" width={64} height={64} />
       <p className="text-[44px]">Handcrafted Haven</p>
     </div>
   );
