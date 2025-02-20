3db22136890364a7edee16639d7df0de46f827ca
supercoolfire <bahuiput+github@gmail.com>
2025-02-20 13:37:23 +0800

    Removing hydration error in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-import { Metadata } from 'next';
- 
-export const metadata: Metadata = {
-  title: 'Hand Crafted Haven',
-};
+// import { Metadata } from 'next';
+import HandLogo from "./ui/hch-logo"; 
+// export const metadata: Metadata = {
+//   title: 'Hand Crafted Haven',
+// };
 
  
 export default async function Page() {
   return (
     <main>
       <h1 className='home-title'>
-        Hand Crafted Haven
+        {/* Hand Crafted Haven */}
+        <HandLogo />
       </h1>
       <div className='enter-button'><a href="/market">Enter</a></div>
     </main>
--- app/ui/hch-logo.tsx
+++ app/ui/hch-logo.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
 
@@ -5,7 +7,7 @@ export default function HandLogo() {
   return (
     <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`} >
       <Image src="/logo.png" alt="Logo" width={64} height={64} />
-      <p className="text-[44px]">Handcrafted Haven</p>
+      <p className="text-[44px]">Hand Crafted Haven</p>
     </div>
   );
 }
